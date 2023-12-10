d266224e72ed3cf53f9c6a3bc7a77e2dd4b8d80c
mohammadhosien <rastegarmohammadhosien@gmail.com>
2023-12-10 23:12:58 +0330

    final version

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -142,99 +142,78 @@ class OrderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //گام اول : محسابه قیمت نهایی
-        // $total_price = 0;
 
-        // $order_products = DB::table('order_products')->get();
-
-        // foreach ($request->all() as $key => $product_count) {
-
-
-        //     if (Str::is('Product*', $key)) {
-
-        //         $product_id = substr($key, -1);
-        //         $order_products = DB::table('order_products')->where('order_id', $id)->get();
-
-        //         foreach ($order_products as $order_product) {
-
-        //                 if ($order_product->product_id == $product_id) {
-        //                     if ($order_product->count != $product_count) {
-
-        //                         $products = DB::table('products')->where('id', $product_id)->first();
-        //                         $total_price += $products->price * $product_count;
-        //                         //پایان عملیات گام اول
-
-
-        //                         DB::table('order_products')->where('product_id', $order_product->product_id)->update([
-        //                             'count' => $product_count,
-        //                         ]);
-
-        //                         $product_inventory = ($products->inventory - $product_count);
-        //                         $product_sold_number = ($products->sold_number + $product_count);
-
-        //                         DB::table('products')->where('id', $product_id)->update([
-        //                             'inventory' => $product_inventory,
-        //                             'sold_number' => $product_sold_number,
-        //                         ]);
-        //                         break;
-        //                     } else {
-        //                         $products = DB::table('products')->where('id', $product_id)->first();
-        //                         $total_price += $products->price * $product_count;
-        //                     }
-        //                 }
-
-        //         }
-
-
-        //         //گام سوم : تعیین مجدد موجودی درون جدول محصولات
-
-
-        //         //پایان گام سوم
-
-        //     }
-        // }
-
-        // DB::table('orders')->where('id', $id)->update([
-        //     'user_id' => $request->user_id,
-        //     'total_price' => $total_price,
-        //     'updated_at' => date('Y-m-d H:i:s'),
-
-        // ]);
-
-
-        // return redirect('/orders');
-        
         $total_price = 0;
-        $title = Order::where('id', $id)->first()->title;
+
         foreach ($request->all() as $key => $product_count) {
+
             if (Str::is('Product*', $key)) {
-                for ($i = 0; $i < $product_count; $i += 1) {
-                    $product_id = substr($key, -1);
+
+                $product_id = substr($key, -1);
+                $order_products = OrderProduct::where(['order_id'=> $id, 'product_id' => $product_id])->get()->first();
+
+                if ($order_products->count != $product_count) {
+
                     $products = Product::where('id', $product_id)->first();
-                    $total_price += ($products->price);
+                    $total_price += $products->price * $product_count;
+                    //پایان عملیات گام اول
 
 
-                    OrderProduct::where('product_id' , $product_id)->update([
+                    OrderProduct::where('product_id', $order_products->product_id)->update([
                         'count' => $product_count,
                     ]);
-                }
-                $product_inventory = ($products->inventory - $product_count);
 
-                Product::where('id', $product_id)->update([
-                    'inventory' => $product_inventory,
-                ]);
+                } else {
+                    $products = Product::where('id', $product_id)->first();
+                    $total_price += $products->price * $product_count;
+                }
             }
+
+
+
         }
 
         Order::where('id', $id)->update([
             'user_id' => $request->user_id,
-            'title' => $title,
             'total_price' => $total_price,
             'updated_at' => date('Y-m-d H:i:s'),
+
         ]);
 
 
         return redirect('/orders');
+
+        // $total_price = 0;
+        // $title = Order::where('id', $id)->first()->title;
+        // foreach ($request->all() as $key => $product_count) {
+        //     if (Str::is('Product*', $key)) {
+        //         for ($i = 0; $i < $product_count; $i += 1) {
+        //             $product_id = substr($key, -1);
+        //             $products = Product::where('id', $product_id)->first();
+        //             $total_price += ($products->price);
+
+
+        //             OrderProduct::where('product_id' , $product_id)->update([
+        //                 'count' => $product_count,
+        //             ]);
+        //         }
+        //         $product_inventory = ($products->inventory - $product_count);
+
+        //         Product::where('id', $product_id)->update([
+        //             'inventory' => $product_inventory,
+        //         ]);
+        //     }
+        // }
+
+        // Order::where('id', $id)->update([
+        //     'user_id' => $request->user_id,
+        //     'title' => $title,
+        //     'total_price' => $total_price,
+        //     'updated_at' => date('Y-m-d H:i:s'),
+        // ]);
+
+
+        // return redirect('/orders');
     }
 
 
